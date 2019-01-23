dce990db76a489e75e60776b559f194ef983a1fc
Laura Royle <lauraamie.royle@gmail.com>
2019-01-23 17:21:24 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -5,3 +5,27 @@ end
 def array_with_two_elements
   Array.new(2)
 end
+
+def first_element(array)
+  Array[0]
+end
+
+def third_element(array)
+  Array[2]
+end
+
+def last_element(array)
+  Array.[-1]
+end
+
+def first_element_with_array_methods(array)
+  Array.first
+end
+
+def last_element_with_array_methods(array)
+  Array.last
+end
+
+def length_of_array
+  Array.length
+end
