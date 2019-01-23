39f60db146d98aee50d3ed4f5550bdf46391c5d6
Laura Royle <lauraamie.royle@gmail.com>
2019-01-23 17:24:07 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -3,15 +3,15 @@ def instantiate_new_array
 end
 
 def array_with_two_elements
-  Array.new(2)
+  array.new(2)
 end
 
 def first_element(array)
-  Array[0]
+  array[0]
 end
 
 def third_element(array)
-  Array[2]
+  array[2]
 end
 
 def last_element(array)
